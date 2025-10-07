64273bf496b50a68cdb9b47e478714841fa48a11
ultron-probot <smitjhn392@gmail.com>
2025-10-07 20:38:04 +0545

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+API_ID = int(os.getenv("API_ID", 12345))
+API_HASH = os.getenv("API_HASH", "your_api_hash")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+CACHE_GROUP_ID = int(os.getenv("CACHE_GROUP_ID", "-1001234567890"))
